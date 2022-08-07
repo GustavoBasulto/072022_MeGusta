f2b4a8593b4e2706bd3a23e4ee7c4d98b33f7b29
Gustavo Basulto <gbasulto.guzman@gmail.com>
2022-08-06 23:00:01 -0400

    carga de archivos inicial

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+
+
+function like1() {
+    var sum = parseInt(document.getElementById("like1").innerHTML);
+    sum=sum+1;
+    document.getElementById("like1").innerHTML=sum;
+    }
+function like2() {
+    var sum = parseInt(document.getElementById("like2").innerHTML);
+    sum=sum+1;
+    document.getElementById("like2").innerHTML=sum;
+    }
+function like3() {
+    var sum = parseInt(document.getElementById("like3").innerHTML);
+    sum=sum+1;
+    document.getElementById("like3").innerHTML=sum;
+    }
